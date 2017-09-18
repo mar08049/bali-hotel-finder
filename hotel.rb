1ca9f0823c48bccc4850c24f26b7cd8356155bde
Cole Martin <mar08049@gmail.com>
2017-09-18 20:11:37 +0000

    updated CLI, hotel.rb, and scraper

--- hotel.rb
+++ hotel.rb
@@ -1,22 +1,14 @@
 class BaliHotelFinder::Hotel
 
-  attr_accessor :name, :location, :description
+  attr_accessor :name, :location, :hotel_class, :contact, :description
 
   @@all = []
 
-  def self.new_from_index_page(i)
-    self.new(
-      i.css("h2").text,
-      "https://travel.usnews.com/Hotels/Bali_Indonesia/#{i.css("a").attribute("href").text}",
-      i.css("h3").text
-      i.css(".location").text
-      )
-  end
-
-  def initialize(name=nil, url=nil, location=nil)
+  def initialize(name, hotel_class, location, description)
     @name = name
-    @url = url
+    @hotel_class = hotel_class
     @location = location
+    @description = description
     @@all << self
   end
 
@@ -24,19 +16,5 @@ class BaliHotelFinder::Hotel
     @@all
   end
 
-  def self.find(id)
-    self.all[id-1]
-  end
 
-  def location
-    @best_dish ||= doc.xpath("//div[@class=text-loose hero-ranking-data-contact block-normal text-small]").text
-  end
-
-  def description
-    @description ||= doc.xpath("//div[#{id=profile-overview}]").text
-  end
-
-  def doc
-    @doc ||= Nokogiri::HTML(open(self.url))
-  end
 end
--- scraper.rb
+++ scraper.rb
@@ -1,15 +1,21 @@
 class BaliHotelFinder::Scraper
 
   def get_page
-    Nokogiri::HTML(open("https://travel.usnews.com/Hotels/Bali_Indonesia/"))
+    doc = Nokogiri::HTML(open("https://travel.usnews.com/Hotels/Bali_Indonesia/"))
   end
 
-  def scrape_hotels_index
-    self.get_page.css("a#{ellipsis-text search-results-link}")
-  end
-  def make_hotels
-    scrape_hotels_index.each do |i|
-      BaliHotelFinder::Hotel.new_from_index_page(i)
-    end
-  end
+  def create_hotel_hash
+    html = Nokogiri.read('https://travel.usnews.com/Hotels/Bali_Indonesia/')
+    hotels = Nokogiri::HTML(html)
+
+    hotels = {}
+
+    hotels.css.each do |hotel|
+      name = hotel.css("h3.ellipsis-text search-results-link").text
+      hotel = {
+        :description => hotel.css("div#profile-overview").text,
+        :hotel_class => hotel.css("div.media-body").text,
+        :contact => project.css("p.text-loose hero-ranking-data-contact-block-normal text-small").text
+      }
+   end
 end
