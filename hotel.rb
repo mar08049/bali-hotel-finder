14a3d7b61c2dc87667a12c7ee76a24a776919838
Cole Martin <mar08049@gmail.com>
2017-09-18 20:39:41 +0000

    hotel.rb modified

--- hotel.rb
+++ hotel.rb
@@ -2,19 +2,11 @@ class BaliHotelFinder::Hotel
 
   attr_accessor :name, :location, :hotel_class, :contact, :description
 
-  @@all = []
-
   def initialize(name, hotel_class, location, description)
     @name = name
     @hotel_class = hotel_class
     @location = location
     @description = description
-    @@all << self
   end
-
-  def self.all
-    @@all
-  end
-
-
+  
 end
