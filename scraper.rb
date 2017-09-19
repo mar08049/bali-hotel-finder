3eb9ef60e90fff8548ddcf72715e4e5d4d896746
Cole <mar08049@gmail.com>
2017-09-18 20:50:16 -0500

    Update scraper.rb
    updated hotel index method

--- scraper.rb
+++ scraper.rb
@@ -4,18 +4,14 @@ class BaliHotelFinder::Scraper
     doc = Nokogiri::HTML(open("https://travel.usnews.com/Hotels/Bali_Indonesia/"))
   end
 
-  def create_hotel_hash
-    html = Nokogiri.read('https://travel.usnews.com/Hotels/Bali_Indonesia/')
-    hotels = Nokogiri::HTML(html)
-
-    hotels = {}
-
-    hotels.css.each do |hotel|
-      name = hotel.css("h3.ellipsis-text search-results-link").text
-      hotel = {
-        :description => hotel.css("div#profile-overview").text,
-        :hotel_class => hotel.css("div.media-body").text,
-        :contact => project.css("p.text-loose hero-ranking-data-contact-block-normal text-small").text
-      }
+    def hotel_index
+    html = self.get_page
+    hotels_array = html.css("h1.hero-heading flex-media-heading block-tight hero-heading text-tighter block-normal flex-media-heading").text
+    hotels_array.each do |hotel|
+      hotels = BaliHotelFinder::Hotel.new
+      hotels.name = hotel.css("h3.ellipsis-text search-results-link").text
+      hotels.description = hotel.css("div#profile-overview").text
+      hotels.hotel_class = hotel.css("div.media-body").text
+      hotels.contact = hotel.css("p.text-loose hero-ranking-data-contact-block-normal text-small").text
    end
 end
